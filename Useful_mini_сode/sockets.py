c4867a5308cb7253445d2e06906e2746dbfdda9c
Teodors Chaly <iteo.chaly@gmail.com>
2023-11-11 12:29:44 +0200

    TCP Chat with GUI

--- Useful_mini_сode/sockets.py
+++ Useful_mini_сode/sockets.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+
 # In programming, a socket is a way for computer programs to exchange information over the internet.
 # To start this app, run sockets.py and clients.py simultaneously
 
@@ -6,8 +8,30 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s. bind(('127.0.0.1', 55555))
 s.listen()
 
-while True:
-    client, address = s.accept()
-    print(f"Connected to {address}")
-    client.send("You are connected!" .encode())
-    client.close()
+clients = []
+nicnames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    pass
+
+def receive():
+    while True:
+        client_communication, address = s.accept()
+        print(f"Connected with {str(address)}")
+
+        client_communication.send("Nick".encode('utf-8'))
+        nickname = client_communication.recv(1024)
+
+        nicnames.append(nickname)
+        clients.append(client_communication)
+
+        print(f"Nickname of the client us {nickname}")
+        broadcast(f"Hello to {nickname}!\n".encode('utf-8'))
+        client_communication.send("Welcome to server".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client_communication, ))
+        thread.start()
